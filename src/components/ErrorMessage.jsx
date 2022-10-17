742be3e07d763780326e457ff97c301d149428d7
Chris <christopherhoganjr@icloud.com>
2022-10-16 19:59:59 -0700

    fixed spelling

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const ErrorMessage = () => {
   return (
-    <h3 className="text-center">
-      That pokemon does not exist or you did not spell the night right.
+    <h3 className='text-center'>
+      That pokemon does not exist or you did not spell the pokemon name right.
     </h3>
   );
 };
